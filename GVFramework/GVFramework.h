4a1d99526453e4e171a7e34ea113ff12453fadca
stuartjmoore <stuartjmoore@gmail.com>
2014-04-15 06:51:14 -0400

    Initial Commit

---
+++ GVFramework/GVFramework.h
@@ -0,0 +1,13 @@
+//
+//  GVFramework.h
+//  GVFramework
+//
+//  Created by Stuart Moore on 4/15/14.
+//  Copyright (c) 2014 Stuart J. Moore. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GVFramework : NSObject
+
+@end
---
+++ GVFramework/GVFramework.m
@@ -0,0 +1,13 @@
+//
+//  GVFramework.m
+//  GVFramework
+//
+//  Created by Stuart Moore on 4/15/14.
+//  Copyright (c) 2014 Stuart J. Moore. All rights reserved.
+//
+
+#import "GVFramework.h"
+
+@implementation GVFramework
+
+@end
