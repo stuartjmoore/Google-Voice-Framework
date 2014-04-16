505373782b3b20d3a2fbbfe13c599c788ff9c8c1
stuartjmoore <stuartjmoore@gmail.com>
2014-04-15 20:00:51 -0400

    Better date parsing. Get messageWithId: from inbox.

--- GVFramework/GVUser.m
+++ GVFramework/GVUser.m
@@ -16,12 +16,21 @@
 @interface GVUser ()
 
 @property (nonatomic, strong) NSString *auth, *sid, *lsid, *r;
+@property (nonatomic, strong) NSOrderedSet *inbox;
 
 @end
 
 #pragma mark -
 
 @implementation GVUser
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        self.inbox = [NSOrderedSet new];
+    }
+    return self;
+}
 
 - (void)login {
     NSDictionary *dictionary = [GVConnection loginWithUsername:self.username andPassword:self.password];
@@ -44,59 +53,92 @@
     self.r = dictionary[@"r"];
 
     NSArray *messageList = dictionary[@"messageList"];
+    NSMutableOrderedSet *messages = [NSMutableOrderedSet new];
 
     for(NSDictionary *messageDict in messageList) {
         GVMessageType type = [messageDict[@"type"] integerValue];
+        NSString *identifier = messageDict[@"id"];
+
         GVMessage *message;
 
         if(type == GVMessageTypeTextReceived || type == GVMessageTypeTextSent)
         {
-            NSLog(@"GVMessageTypeConversation");
-            message = [GVConversation new];
+            message = [self messageWithId:identifier] ?: [GVConversation new];
 
             NSArray *children = messageDict[@"children"];
-            NSMutableArray *textMessages = [NSMutableArray new];
+            NSMutableOrderedSet *textMessages = [NSMutableOrderedSet new];
 
             for(NSDictionary *textMessageDict in children) {
                 GVTextMessage *textMessage = [GVTextMessage new];
                 textMessage.identifier = textMessageDict[@"id"];
-                textMessage.date = [NSDate dateWithTimeIntervalSince1970:(NSTimeInterval)([textMessageDict[@"startTime"] longLongValue] / 1000.0f)];
                 textMessage.text = textMessageDict[@"message"];
+                textMessage.read = [textMessageDict[@"isRead"] boolValue];
+
+                NSInteger milliseconds = [textMessageDict[@"startTime"] integerValue] ?: 0;
+                NSTimeInterval seconds = (NSTimeInterval)(milliseconds / 1000.0);
+                textMessage.date = [NSDate dateWithTimeIntervalSince1970:seconds];
 
                 textMessage.conversation = (GVConversation*)message;
                 [textMessages addObject:textMessage];
             }
 
-            ((GVConversation*)message).textMessages = textMessages;
+            [textMessages unionOrderedSet:((GVConversation*)message).textMessages];
+            [textMessages sortUsingComparator:^NSComparisonResult(GVMessage *obj1, GVMessage *obj2) {
+                return obj2.date == [obj1.date earlierDate:obj2.date];
+            }];
+
+            ((GVConversation*)message).textMessages = [NSOrderedSet orderedSetWithOrderedSet:textMessages];
         }
         else if(type == GVMessageTypeMissedCall)
         {
-            NSLog(@"GVMessageTypeMissedCall");
-            message = [GVMissedCall new];
+            message = [self messageWithId:identifier] ?: [GVMissedCall new];
+            message.read = [messageDict[@"isRead"] boolValue];
+
+            NSInteger milliseconds = [messageDict[@"startTime"] integerValue] ?: 0;
+            NSTimeInterval seconds = (NSTimeInterval)(milliseconds / 1000.0);
+            message.date = [NSDate dateWithTimeIntervalSince1970:seconds];
 
         }
         else if(type == GVMessageTypeVoicemail)
         {
-            NSLog(@"GVMessageTypeVoicemail");
-            message = [GVVoicemail new];
+            message = [self messageWithId:identifier] ?: [GVVoicemail new];
             message.text = messageDict[@"messageText"];
+            message.read = [messageDict[@"isRead"] boolValue];
             ((GVVoicemail*)message).duration = [messageDict[@"duration"] integerValue];
+
+            NSInteger milliseconds = [messageDict[@"startTime"] integerValue] ?: 0;
+            NSTimeInterval seconds = (NSTimeInterval)(milliseconds / 1000.0);
+            message.date = [NSDate dateWithTimeIntervalSince1970:seconds];
         }
 
-        message.identifier = messageDict[@"id"];
-        message.date = [NSDate dateWithTimeIntervalSince1970:(NSTimeInterval)([messageDict[@"startTime"] longLongValue] / 1000.0f)];
+        message.identifier = identifier;
         message.note = messageDict[@"note"];
         message.labels = messageDict[@"labels"];
-        message.read = [messageDict[@"isRead"] boolValue];
         message.spam = [messageDict[@"isSpam"] boolValue];
         message.trash = [messageDict[@"isTrash"] boolValue];
         message.starred = [messageDict[@"star"] boolValue];
+
+        [messages addObject:message];
 
         NSLog(@"message: %@", message);
         NSLog(@"-------");
     }
 
-    NSLog(@"messages: %@", dictionary);
+    [messages unionOrderedSet:self.inbox];
+    [messages sortUsingComparator:^NSComparisonResult(GVMessage *obj1, GVMessage *obj2) {
+        return obj1.date == [obj1.date earlierDate:obj2.date];
+    }];
+
+    self.inbox = [NSOrderedSet orderedSetWithOrderedSet:messages];
+
+    NSLog(@"self.inbox: %@", self.inbox);
+}
+
+- (GVMessage*)messageWithId:(NSString*)identifier {
+    NSPredicate *pred = [NSPredicate predicateWithFormat:@"identifier == %@", identifier];
+    NSOrderedSet *filteredOrderedSet = [self.inbox filteredOrderedSetUsingPredicate:pred];
+
+    return filteredOrderedSet.set.anyObject;
 }
 
 @end
